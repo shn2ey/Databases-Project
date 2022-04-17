0c3da22d99452adfb9bb2ec2257672e0985f5b83
tibaalshammari <103067958+tibaalshammari@users.noreply.github.com>
2022-04-17 10:13:06 -0400

    Add files via upload

---
+++ assets/db_conn.php
@@ -0,0 +1,10 @@
+<?php
+
+/** S22, PHP (on GCP, local XAMPP, or CS server) connect to MySQL (on local XAMPP) **/
+$username = 'cs4750user';
+$password = 'Saroon98';
+$host = 'localhost:3306';
+$dbname = 'tibadb';
+$conn = "mysql:host=$host;dbname=$dbname";
+
+?>
