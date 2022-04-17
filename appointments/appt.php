aac6a2353bc05c753b93db1b98c2500a499659b3
tibaalshammari <103067958+tibaalshammari@users.noreply.github.com>
2022-04-17 18:01:23 -0400

    Add files via upload

---
+++ appointments/appt.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <meta name="copyright" content="MACode ID, https://macodeid.com/">
+
+  <title>MedPort Insurance</title>
+
+  <link rel="stylesheet" href="../assets/css/maicons.css">
+
+  <link rel="stylesheet" href="../assets/css/bootstrap.css">
+
+  <link rel="stylesheet" href="../assets/vendor/owl-carousel/css/owl.carousel.css">
+
+  <link rel="stylesheet" href="../assets/vendor/animate/animate.css">
+
+  <link rel="stylesheet" href="../assets/css/theme.css">
+</head>
+<body>
+
+  <!-- Back to top button -->
+  <div class="back-to-top"></div>
+
+  <header>
+    
+
+    <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
+      <div class="container">
+        <a class="navbar-brand" href="#"><span class="text-primary">Med</span>-Port</a>
+
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupport" aria-controls="navbarSupport" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupport">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="about.html">Home</a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link" href="MedicalPassport.html">Medical Passport</a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link" href="checkin.html">Daily Check In</a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link" href="insurance.html">Insurance</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="appointments.html">Appointments</a>
+            </li>
+            <li class="nav-item">
+              <a class="btn btn-primary ml-lg-3" href="#">Logout</a>
+            </li>
+
+          </ul>
+        </div> <!-- .navbar-collapse -->
+      </div> <!-- .container -->
+    </nav>
+  </header>
+
+  <div class="page-section pt-5">
+    <div class="container">
+      <!-- <div class="row">
+        <div class="col-lg-8">
+          <nav aria-label="Breadcrumb">
+            <ol class="breadcrumb bg-transparent py-0 mb-5">
+              <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+              <li class="breadcrumb-item"><a href="blog.html">Blog</a></li>
+              <li class="breadcrumb-item active" aria-current="page">List of Countries without Coronavirus case</li>
+            </ol>
+          </nav>
+        </div>
+      </div> <!-- .row --> 
+
+      <div class="row">
+        <div class="col-lg-8">
+          <article class="blog-details">
+            <div class="post-thumb">
+              <img src="../assets/img/blog/blog_1.jpg" alt="">
+            </div>
+            <div class="post-meta">
+              <div class="post-author">
+                <span class="text-grey">By</span> <a href="#">Admin</a>  
+              </div>
+              <span class="divider">|</span>
+              <div class="post-date">
+                <a href="#">22 Jan, 2018</a>
+              </div>
+              <span class="divider">|</span>
+              <div>
+                <a href="#">StreetStyle</a>, <a href="#">Fashion</a>, <a href="#">Couple</a>  
+              </div>
+              <span class="divider">|</span>
+              <div class="post-comment-count">
+                <a href="#">8 Comments</a>
+              </div>
+            </div>
+            <h2 class="post-title h1">My Complete History</h2>
+            <div class="post-content">
+              <p>IDK how we want to gather these things from the database
+
+              <p> </p>
+              <p> </p>     
+              <p> </p>     
+              <p> </p>
+
+              let me know if you want a search bar here
+
+
+              </p>
+
+              <p>
+              good luck
+              </p>
+            </div>
+           
+          </article> <!-- .blog-details -->
+
+         
+        </div>
+        <div class="col-lg-4">
+          <div class="sidebar">
+            <!-- <div class="sidebar-block">
+              <h3 class="sidebar-title">Search</h3>
+              <form action="#" class="search-form">
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="Type a keyword and hit enter">
+                  <button type="submit" class="btn"><span class="icon mai-search"></span></button>
+                </div>
+              </form>
+            </div> -->
+            <div class="sidebar-block">
+              <h3 class="sidebar-title">Your Medical Conditions</h3>
+              <ul class="categories">
+                <!-- for loop
+                  if person has medical conditions, put them here 
+                    otherwise, say no medical conditions -->
+                <li><a href="#">You </a></li>
+                <li><a href="#">Are </a></li>
+                <li><a href="#">Busted </a></li>
+
+              </ul>
+            </div>
+
+            <div class="sidebar-block">
+              <h3 class="sidebar-title">Upcoming Appointments</h3>
+              <div class="blog-item">
+               
+                <div class="content">
+                  <h5 class="post-title"><a href="#">Medical Appointment</a></h5>
+                  <div class="meta">
+                    <!-- get dates from the database,
+                    if the date of the appintment is after today, display it here -->
+                    <a href="#"><span class="mai-calendar"></span> July 12, 2018</a>
+                    <a href="#"><span class="mai-chatbubbles"></span> 19</a>
+
+                  </div>
+                </div>
+              </div>
+              <div class="blog-item">
+               
+                <div class="content">
+                  <h5 class="post-title"><a href="#">Medical Appointment</a></h5>
+                  <div class="meta">
+                    <a href="#"><span class="mai-calendar"></span> July 12, 2018</a>
+                    <a href="#"><span class="mai-chatbubbles"></span> 19</a>
+                  </div>
+                </div>
+              </div>
+              <div class="blog-item">
+               
+                <div class="content">
+                  <h5 class="post-title"><a href="#">Medical Appointment</a></h5>
+                  <div class="meta">
+                    <a href="#"><span class="mai-calendar"></span> July 12, 2018</a>
+                    <a href="#"><span class="mai-chatbubbles"></span> 19</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- <div class="sidebar-block">
+              <h3 class="sidebar-title">Tag Cloud</h3>
+              <div class="tagcloud">
+                <a href="#" class="tag-cloud-link">dish</a>
+                <a href="#" class="tag-cloud-link">menu</a>
+                <a href="#" class="tag-cloud-link">food</a>
+                <a href="#" class="tag-cloud-link">sweet</a>
+                <a href="#" class="tag-cloud-link">tasty</a>
+                <a href="#" class="tag-cloud-link">delicious</a>
+                <a href="#" class="tag-cloud-link">desserts</a>
+                <a href="#" class="tag-cloud-link">drinks</a>
+              </div>
+            </div> -->
+
+            <div class="sidebar-block">
+              <h3 class="sidebar-title">Latest Feedback</h3>
+              <p>
+                if doctor gave feedback (it is a variable in teh database) display the message here, othereise say there are no messages 
+              </p>
+            </div>
+          </div>
+        </div> 
+      </div> <!-- .row -->
+    </div> <!-- .container -->
+  </div> <!-- .page-section -->
+<div class="page-section banner-home bg-image" style="background-image: url(../assets/img/banner-pattern.svg);">
+    <div class="container py-5 py-lg-0">
+      <div class="row align-items-center">
+        <div class="col-lg-4 wow zoomIn">
+          <div class="img-banner d-none d-lg-block">
+            <img src="../assets/img/mobile_app.png" alt="">
+          </div>
+        </div>
+        <div class="col-lg-8 wow fadeInRight">
+          <h1 class="font-weight-normal mb-3">Get easy access of all features using the MedPort Application</h1>
+          <a href="#"><img src="../assets/img/google_play.svg" alt=""></a>
+          <a href="#" class="ml-2"><img src="../assets/img/app_store.svg" alt=""></a>
+        </div>
+      </div>
+    </div>
+  </div> <!-- .banner-home -->
+
+  <footer class="page-footer">
+
+    </div>
+  </footer>
+
+<script src="../assets/js/jquery-3.5.1.min.js"></script>
+
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+
+<script src="../assets/vendor/owl-carousel/js/owl.carousel.min.js"></script>
+
+<script src="../assets/vendor/wow/wow.min.js"></script>
+
+<script src="../assets/js/theme.js"></script>
+  
+</body>
+</html>
