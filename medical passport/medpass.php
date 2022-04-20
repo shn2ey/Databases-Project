e4944f5b8a611819d6ffd1db9e073c6f09034306
tibaalshammari <103067958+tibaalshammari@users.noreply.github.com>
2022-04-19 21:53:33 -0400

    Update medpass.php

--- medical passport/medpass.php
+++ medical passport/medpass.php
@@ -1,3 +1,37 @@
+<?php 
+session_start();
+require('db_conn.php');
+include "edit.php";
+//require("updatemedInsur.php");
+
+// $updating = null;
+// $list=getAllInsurance();
+
+// if ($_SERVER['REQUEST_METHOD'] == 'POST')
+// {
+//     if (!empty($_POST['btnAction']) && $_POST['btnAction'] == "Edit")
+//     {  
+//       $updating = getInsurance($_POST['updating']);
+//     }
+//     elseif (!empty($_POST['btnAction']) && $_POST['btnAction'] == "Confirm update"){ 
+//       updateInsurance($_POST['insurance_plan']);
+//       $list=getAllInsurance();
+//     //updateAddress($_POST['address']);
+//     }
+//   }
+// if($_SERVER['REQUEST_METHOD'] == 'POST'){
+//   if (!empty($_POST['btnAction']) && $_POST['btnAction'] == "submit"){
+//     $query = "SELECT * from patient_insurance_plan";
+//     $result = mysqli_query($query);
+//     $updateInsurance = "UPDATE patient_insurance_plan SET insurance_plan = 'updateInsurance()' WHERE patient_ID='$user_ID'";
+//     $result1 = mysqli_query($updateInsurance);
+//     $result1 = $_SESSION['insurance_plan'];
+
+//   }
+if (isset($_SESSION['user_ID'])) {
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -40,24 +74,27 @@
         <div class="collapse navbar-collapse" id="navbarSupport">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <a class="nav-link" href="about.html">Home</a>
+              <a class="nav-link" href="about.php">Home</a>
             </li>
             <li class="nav-item active">
-              <a class="nav-link" href="MedicalPassport.html">Medical Passport</a>
+              <a class="nav-link" href="MedicalPassport.php">Medical Passport</a>
             </li>
             <li class="nav-item ">
-              <a class="nav-link" href="checkin.html">Daily Check In</a>
+              <a class="nav-link" href="checkin.php">Daily Check In</a>
             </li>
             <li class="nav-item ">
-              <a class="nav-link" href="insurance.html">Insurance</a>
+              <a class="nav-link" href="insurance.php">Insurance</a>
             </li>
             <li class="nav-item ">
-              <a class="nav-link" href="appointments.html">Appointments</a>
+              <a class="nav-link" href="appointments.php">Appointments</a>
             </li>
             <li class="nav-item">
-              <a class="btn btn-primary ml-lg-3" href="#">Logout</a>
+              <a class="btn btn-primary ml-lg-3" href="logout.php">Logout</a>
             </li>
           </ul>
+
+
+
         </div> <!-- .navbar-collapse -->
       </div> <!-- .container -->
     </nav>
@@ -68,7 +105,7 @@
       <div class="container text-center wow fadeInUp">
         <nav aria-label="Breadcrumb">
           <ol class="breadcrumb breadcrumb-dark bg-transparent justify-content-center py-0 mb-2">
-            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+            <li class="breadcrumb-item"><a href="index.php">Home</a></li>
             <li class="breadcrumb-item active" aria-current="page">Medical Passport</li>
           </ol>
         </nav>
@@ -77,100 +114,123 @@
     </div> <!-- .banner-section -->
   </div> <!-- .page-banner -->
 
-  <?php
-    require('updateMed.php')
-    require('updateMedInsur.php')
-    require('updateMedCond.php')
-
-    if(is_array($fetchData)){
-      foreach($fetchData as $data){}}
-    elseif(is_array($fetchData2)){
-      foreach)$fetchData2 as $data2){}}
-    elseif(is_array($fetchData3)){
-      foreach)$fetchData3 as $data3){}}
-    else if (!empty($_POST['btnAction']) && $_POST['btnAction'] == "Edit"){
-      if ($_POST['insurance_plan']){
-        updateInsurance($_POST['insurance_plan'])
-      }
-      elseif ($_POST['phone_number']){
-        updatePhone($_POST['phone_number'])
-      }
-      elseif ($_POST['address']) {
-        updateAddress($_POST['address'])
-      }
-    }
-    ?>
 
   <div class="page-section">
     <div class="container">
 
         <div class="row mt-5 ">
           <div class="col-12 col-sm-6 py-2 wow fadeInRight">
-            <p> Patient ID: 
-            <?php 
-              //echo $data['user_ID']
-             if (isset($_POST['id'])){
-                 $mysqli = new mysqli("localhost", "test", "test", "test");
-                if ($mysqli->connect_errno) {
-                  print "Failed to connect to MySQL: " . $mysqli->connect_error;
-                 }
-                $id = $mysqli->real_escape_string($_POST['id']);
-                $res = $mysqli->query("SELECT patient_id FROM users WHERE id = $id");
-                $row = $res->fetch_assoc();
-                print '<p>' . html_entities($row['username']) .'</p>';
-              }
-            ?>
-            </p> 
+            <p> Patient ID: <?php echo $_SESSION['user_ID']; ?></p> <!-- # get from database  -->
           </div>
           <div class="col-12 col-sm-6 py-2 wow fadeInLeft">
-            <p> Name:  <!-- # get from database  -->
-            <td> <?php echo $data['first_name'] && $data['last_name']??''; ?> </td> 
-            </p>
+            <p> Name: <?php echo $_SESSION['first_name']; ?> <?php echo $_SESSION['last_name']; ?></p> <!-- # get from database  -->
           </div>
           <div class="col-12 col-sm-6 py-2 wow fadeInRight">
-            <p> Age:  <!-- # get from database  -->
-            <td><?php echo $data['birthdate']??''; ?></td>
-            </p>
+            <p> Age: </p> <!-- # get from database  -->
           </div>
           <div class="col-12 col-sm-6 py-2 wow fadeInLeft" data-wow-delay="300ms">
-            <form action="medPass.php" method="post">
-            <p> Phone Number: 
-              <input type="submit" name ='btnAction' value="Edit" class="btn btn-primary" >
-              <input type="hidden" value=<?php echo $data['phone_number'] ??''; ?> >
-            </p>
-          </form>
+            <p> Email: <?php echo $_SESSION['email']; ?> <button type="submit" class="btn btn-primary">Edit</button> </p> <!-- # get from database  -->
           </div>
           <div class="col-12 col-sm-6 py-2 wow fadeInRight">
-          <form action="medPass.php" method="post">
-            <p> Address:
-            <input type="submit" name = 'btnAction' value="Edit" class="btn btn-primary" >
-            <input type="hidden" value=<?php echo $data['address'] ??''; ?>>
-            </p>
-          </form>
+            <p> Address: <?php echo $_SESSION['address']; ?>   
+            <button type="submit" class="btn btn-primary">Edit</button> </p> <!-- # get from database  -->
+
           </div>
+          <!-- <form method="post" -->>
           <div class="col-12 col-sm-6 py-2 wow fadeInLeft" data-wow-delay="300ms">
-            <form action="medPass.php" method="post">
             <p> Insurance Plan: 
-              <input type="submit" name = 'btnAction' value="Edit" class="btn btn-primary" >
-              <input type="hidden" value=<?php echo $data['insurance_plan'] ??''; ?> >
-            </form>
-            </p>
+              <?php
+                $count = 1;
+                $user_ID = $_SESSION['user_ID'];
+                $query = "SELECT * FROM patient_insurance_plan WHERE patient_ID='$user_ID'";
+                $insurance_plan = $_SESSION['insurance_plan'];
+                if ($result = mysqli_query($conn, $query)) {
+                  while ($row = mysqli_fetch_array($result)) {
+                      echo $row["insurance_plan"];
+                    }
+                  mysqli_free_result($result);
+                }
+
+                $sql = "SELECT * from patient_insurance_plan";
+                $results= $conn->query($sql);
+                if ($results->num_rows > 0) {
+                  while($row2 = $results->fetch_assoc()) {
+              ?>
+              <a href="edit.php?id=<?php echo $row["user_ID"]; ?>">Edit</a>
+              <?php $count++;  ?>
+            <!-- <button type="submit" onClick = "updateInsurance()" name = "btnAction" class="btn btn-primary">Edit</button> -->
+          <!--   <tr>
+            <td>?=$row2['insurance_plan'];?></td>
+            <td><button type="button" class="btn btn-primary update" data-toggle="modal" data-keyboard="false" data-backdrop="static" data-target="#update_country"
+            data-id="?=$row2['insurance_plan'];?>">Edit</button></td>
+            <input type="text" name="insurance_plan_modal" id="insurance_plan_modal" class="form-control-sm" required>
+            </tr> -->
+            <?php
+          }
+        }
+            ?>
+            <!-- <form action="">
+            <button type="submit" onClick = "updateInsurance(this.value)"name = "btnAction" class="btn btn-primary">Edit</button>
+            </form> -->
+            
+            <!-- <button type="button" onClick = "updateInsurance()" class="btn btn-primary" >Edit</button> -->
+          <!-- </form> -->
+
+          
+            <!-- <input type="text" class = "form-control" name ="insurance_plan" required value="php if ($updating !=null) echo $updating[insurance_plan] ?>" />
+            <input type="submit" value="Confirm update" name="btnAction" class="btn btn-dark" 
+            title="confirm update" />
+          </form>
+          
+          //foreach ($list as $insurance): ?>
+          <form method="post">
+        <input type="submit" value="Update" name="btnAction" class="btn btn-primary" />
+        <input type="hidden" name="updating" value="//php echo $insurance['insurance_plan'] " />      
+      </form>
+     endforeach; ?> -->
+          <!-- <button type="submit" class="btn btn-primary">Edit</button> -->
+               
+          </p>
+          <!-- # get from database  -->
+              <!-- <input type="text" class="form-control" name="insurance_plan" required 
+              value="<
+              // if ($updating!=null) echo $updating['insurance_plan'] 
+              "
+              />
+              </p> # get from database  -->
+              <!-- <form action= "MedicalPassport.php" method="post">
+              <input type="submit" value = "Edit" name = "btnAction" class="btn btn-primary"/>
+              <input type="hidden" name = "updating" value="echo $patient_insurance_plan['insurance_plan'] ?>"/>
+              </form> -->
+
+
+
+              
           </div>
+
           <div class="col-12 col-sm-6 py-2 wow fadeInLeft" data-wow-delay="300ms">
             <p> Medical Conditions: 
-            <?php
-            //echo $data3['medical_condition']  ??''
-            $result = mysql_query("SELECT medical_conditions FROM patient_medical_condition");
-            while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-              printf("%s", $row["medical_condition"]);
-            }
-            mysql_free_result($result);
+              <?php
+                    $user_ID = $_SESSION['user_ID'];
+          
+                    $query = "SELECT * FROM patient_medical_condition WHERE patient_ID='$user_ID'";
+                    $medical_condition = $_SESSION['medical_condition'];
+                    if ($result = mysqli_query($conn, $query)) {
+                      while ($row = mysqli_fetch_array($result)) {
+                        echo $row["medical_condition"];
+                      }
+                      mysqli_free_result($result);
+                    }    
             ?>
-            </p> <!-- # get from database  -->
-          </div>
+          </p>
         </div>
-        <button type="submit" class="btn btn-primary wow zoomIn">Export to PDF</button>
-      </div>
+          
+          <form action="exportToCSV.php">
+          <input type="button" value="Download CSV File For My Appointments">
+          </form>
+          <!-- <button type="submit" class="btn btn-primary wow zoomIn">Export to CSV</button> -->
+          
+        </div>
 
     </div> <!-- .container -->
   </div> <!-- .page-section -->
@@ -210,3 +270,84 @@
   
 </body>
 </html>
+
+<script>
+function updateInsurance(insurance_plan){
+  //alert("test");
+  let insurance = prompt("Please enter your new insurance plan");
+  var d = {user_ID = patient_ID, insurance_plan: insurance};
+  $_POST('MedicalPassport.php', d);
+  alert(insurance);
+}
+// function funcEdit(){  
+//   var mysql = require('db_conn.php');
+
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "tiba",
+//   password: "tiba",
+//   database: "tiba"
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   //Update the address field:
+//   let insurance = prompt("Please enter your new insurance plan");
+//   var sql = "UPDATE patient_insurance_plan SET insurance_plan = 'Medicare' WHERE insurance_plan = 'AETNA'";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log(result.affectedRows + " record(s) updated");
+//   });
+// });   
+// }
+</script>
+
+<script>
+$(document).ready(function() {
+  $.ajax({
+    url: "MedicalPassport.php",
+    type: "POST",
+    cache: false,
+    success: function(dataResult){
+      $('#table').html(dataResult); 
+    }
+  });
+  $(function () {
+    $('#update_country').on('show.bs.modal', function (event) {
+      var button = $(event.relatedTarget); /*Button that triggered the modal*/
+      var insurance_plan = button.data('insurance_plan');
+      var modal = $(this);
+      modal.find('#insurance_plan_modal').val(insurance_plan);
+      
+    });
+    });
+  $(document).on("click", "#update_data", function() { 
+    $.ajax({
+      url: "update_ajax.php",
+      type: "POST",
+      cache: false,
+      data:{
+        insurance_plan: $('#insurance_plan_modal').val(),
+      },
+      success: function(dataResult){
+        var dataResult = JSON.parse(dataResult);
+        if(dataResult.statusCode==200){
+          $('#update_country').modal().hide();
+          alert('Data updated successfully !');
+          location.reload();          
+        }
+      }
+    });
+  }); 
+});
+</script>
+<?php 
+
+}else{
+
+     header("Location: index.php");
+
+     exit();
+
+   }
+?>
